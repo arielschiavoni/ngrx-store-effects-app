54cbb168620e0cff8d5a82c64a99e7f76337acd0
Ariel Schiavoni <ariel.schiavoni@bd4travel.com>
2017-12-29 18:18:24 +0100

    Render selected pizza using router state from redux store

--- src/products/store/selectors/pizzas.selectors.ts
+++ src/products/store/selectors/pizzas.selectors.ts
@@ -1,14 +1,21 @@
 import { createSelector } from '@ngrx/store';
 
-import * as rootStore from '../../store';
+import * as rootStore from '../../../app/store';
 import * as feature from '../reducers';
 import * as pizzasReducer from '../reducers/pizzas.reducer';
+import { Pizza } from '../../models/pizza.model';
 
 // we create selectors from the ProductsStates selector
 export const getPizzaState = createSelector(feature.getProductsState, (state: feature.ProductsState) => state.pizzas);
 
 // more selectors composition
 export const getAllPizzasEntities = createSelector(getPizzaState, pizzasReducer.getPizzasEntities);
+
+export const getSelectedPizza = createSelector(
+  getAllPizzasEntities,
+  rootStore.getRouterState,
+  (entities, routerState): Pizza => routerState.state && entities[routerState.state.params.pizzaId]
+);
 
 export const getAllPizzas = createSelector(getAllPizzasEntities, entities =>
   Object.keys(entities).map(id => entities[parseInt(id, 10)])
